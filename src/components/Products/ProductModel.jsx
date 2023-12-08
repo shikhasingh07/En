0f5bf4ed29ac616b4f678186acef4cf8ed7423ad
shikhasingh07 <shikha.thakur2295@gmail.com>
2023-12-08 10:28:55 +0530

    fixing 3d model

--- src/components/Products/ProductModel.jsx
+++ src/components/Products/ProductModel.jsx
@@ -2,46 +2,26 @@ import {
   Float,
   MeshReflectorMaterial,
   PresentationControls,
-  Environment, OrbitControls,
   Stage,
-  useFBX,
+  OrbitControls,
+  Environment
 } from "@react-three/drei";
 import { useLoader } from "@react-three/fiber";
 import { Suspense } from "react";
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 const ProductModel = () => {
-  const fbx = useFBX("../../../public/models/drone1.fbx");
+  const fbx = useLoader(FBXLoader, "../../../public/models/drone1.fbx");
   return (
-    <PresentationControls
-      speed={1.5}
-      global
-      polar={[-0.1, Math.PI / 4]}
-      zoom={0.1}
-    >
+    <PresentationControls speed={1.5} global polar={[-0.1, Math.PI / 4]}>
       <Stage environment="city" intensity={1.2} castShadow={false}>
-        <primitive object={fbx} scale={0.01} />;
-        <OrbitControls />
-        <Environment preset="sunset" background />
+        <Suspense fallback={null}>
+          <OrbitControls />
+          <primitive object={fbx} scale={0.005} />
+          <Environment preset="sunset" background />
+        </Suspense>
       </Stage>
     </PresentationControls>
   );
 };
 
 export default ProductModel;
-{
-  /* <mesh rotation={[-Math.PI / 2, 0, 0]}>
-        <planeGeometry args={[170, 170]} />
-        <MeshReflectorMaterial
-          blur={[300, 100]}
-          resolution={2048}
-          mixBlur={1}
-          mixStrength={40}
-          roughness={1}
-          depthScale={1.2}
-          minDepthThreshold={0.4}
-          maxDepthThreshold={1.4}
-          color="#101010"
-          metalness={0.5}
-        />
-      </mesh> */
-}
